fd2edf9ffe11880605b75062fd613c4aedf217c2
Joscha Loos <j.loos@posteo.net>
2022-11-13 16:30:06 +0100

    chore: rename template, add minimal template

---
+++ plugin_template_minimal.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+let
+
+  name = "PLUGIN_NAME";
+
+  helpers = import ../helpers.nix { inherit lib config; };
+
+  moduleOptions = with helpers; {
+    # add module options here
+  };
+
+in with helpers;
+mkLuaPlugin {
+  inherit name moduleOptions;
+  description = "Enable ${name}.nvim";
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    # nvim-treesitter
+  ];
+  extraPackages = with pkgs; [
+    # add dependencies here
+    # tree-sitter
+  ];
+}
