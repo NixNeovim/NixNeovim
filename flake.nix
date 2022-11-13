fbea016bc251b70f3cf2817ad4e0ebf3d311f6b2
Joscha Loos <j.loos@posteo.net>
2022-11-13 17:21:59 +0100

    feat: testing around with standalone approaches

--- flake.nix
+++ flake.nix
@@ -11,12 +11,14 @@
   outputs = { self, nixpkgs, nmdSrc, vimExtraPlugins, ... }@inputs:
   let
     system = "x86_64-linux";
+
     pkgs = import nixpkgs {
       inherit system;
       overlays = [
         vimExtraPlugins.overlays.default
       ];
     };
+
   in {
     packages.${system}.docs = import ./docs {
       pkgs = pkgs;
@@ -25,6 +27,30 @@
 
     nixosModules.nixvim = import ./nixvim.nix { nixos = true; inherit pkgs; };
     homeManagerModules.nixvim = import ./nixvim.nix { homeManager = true; inherit pkgs; };
+
+    # nixosModules = rec {
+    #   # default = import ./nixvim.nix { nixos = true; inherit pkgs; };
+    #   default = import ./nixvim.nix { nixos = false; inherit pkgs; };
+    # };
+
+    # overlays.default = super: self: {
+    #   nixvim = self.packages.${system}.default;
+    # };
+
+    apps.${system} = {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/nvim";
+      };
+    };
+
+    packages.${system}.default = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+      configure = {
+        customRC = ''
+          set number relativenumber
+        '';
+      };
+    };
 
   };
 }
