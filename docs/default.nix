451e75448a90362557baad257eb502d6e08cc505
Joscha Loos <j.loos@posteo.net>
2022-11-24 17:46:46 +0100

    clean up

--- docs/default.nix
+++ docs/default.nix
@@ -1,15 +1,8 @@
-{ pkgs ? import <nixpkgs> {}
-, lib ? import <nixpkgs/lib>
-, ... }:
+{ pkgs, lib, nmd, ... }:
 let
-  nmdSrc = pkgs.fetchFromGitLab {
-    name = "nmd";
-    owner = "rycee";
-    repo = "nmd";
-    rev = "527245ff605bde88c2dd2ddae21c6479bb7cf8aa";
-    sha256 = "1zi0f9y3wq4bpslx1py3sfgrgd9av41ahpandvs6rvkpisfsqqlp";
-  };
-  nmd = import nmdSrc { inherit pkgs lib; };
+
+  nmd = import nmd { inherit pkgs lib; };
+
   scrubbedPkgsModule = {
     imports = [{
       _module.args = {
@@ -18,6 +11,7 @@ let
       };
     }];
   };
+
   buildModulesDocs = args:
     nmd.buildModulesDocs ({
       moduleRootPaths = [ ./.. ];
@@ -25,6 +19,7 @@ let
         "https://github.com/pta2002/nixvim/blob/master/${path}#blob-path";
       channelName = "nixvim";
     } // args);
+
   nixvimDocs = buildModulesDocs {
     modules = [
       (import ../nixvim.nix { pkgs = pkgs;})
@@ -39,12 +34,12 @@ let
     documentsDirectory = ./.;
     documentType = "book";
     chunkToc = ''
-    <toc>
-      <d:tocentry xmlns:d="http://docbook.org/ns/docbook" linkend="book-home-manager-manual"><?dbhtml filename="index.html"?>
-        <d:tocentry linkend="ch-options"><?dbhtml filename="options.html"?></d:tocentry>
-        <d:tocentry linkend="ch-release-notes"><?dbhtml filename="release-notes.html"?></d:tocentry>
-      </d:tocentry>
-      </toc>
-    '';
+      <toc>
+        <d:tocentry xmlns:d="http://docbook.org/ns/docbook" linkend="book-home-manager-manual"><?dbhtml filename="index.html"?>
+          <d:tocentry linkend="ch-options"><?dbhtml filename="options.html"?></d:tocentry>
+          <d:tocentry linkend="ch-release-notes"><?dbhtml filename="release-notes.html"?></d:tocentry>
+        </d:tocentry>
+        </toc>
+      '';
   };
 in docs.html
