f946b1b0312e1b7204a9ec4978491f747f2da89d
Joscha Loos <j.loos@posteo.net>
2022-11-24 17:46:37 +0100

    ref!: use flake style nixosModules instead of old syntax; clean up

--- flake.nix
+++ flake.nix
@@ -1,51 +1,56 @@
 {
   description = "A neovim configuration system for NixOS";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.nmdSrc.url = "gitlab:rycee/nmd";
-  inputs.nmdSrc.flake = false;
+    nmd = {
+      url = "gitlab:rycee/nmd";
+      flake = false;
+    };
+  };
 
   outputs = { self, nixpkgs, nmdSrc, ... }@inputs:
-  let
-    system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-
-  in {
-    packages.${system}.docs = import ./docs {
-      pkgs = pkgs;
-      lib = nixpkgs.lib;
-    };
-
-    nixosModules.nixvim = import ./nixvim.nix { nixos = true; inherit pkgs; };
-    homeManagerModules.nixvim = import ./nixvim.nix { homeManager = true; inherit pkgs; };
-
-    # nixosModules = rec {
-    #   # default = import ./nixvim.nix { nixos = true; inherit pkgs; };
-    #   default = import ./nixvim.nix { nixos = false; inherit pkgs; };
-    # };
-
-    # overlays.default = super: self: {
-    #   nixvim = self.packages.${system}.default;
-    # };
-
-    apps.${system} = {
-      default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/nvim";
+      pkgs = import nixpkgs {
+        inherit system;
       };
-    };
 
-    packages.${system}.default = pkgs.wrapNeovim pkgs.neovim-unwrapped {
-      configure = {
-        customRC = ''
-          set number relativenumber
-        '';
+    in {
+      packages.${system}.docs = import ./docs {
+        pkgs = pkgs;
+        lib = nixpkgs.lib;
       };
-    };
 
-  };
+      # nixosModules.nixvim = import ./nixvim.nix { };
+      # homeManagerModules.nixvim = import ./nixvim.nix { homeManager = true; };
+
+      nixosModules = rec {
+        default = import ./nixvim.nix { homeManager = true; };
+        homeManager = self.nixosModules.default;
+        nixos = import ./nixvim.nix { homeManager = false; };
+      };
+
+      # overlays.default = super: self: {
+      #   nixvim = self.packages.${system}.default;
+      # };
+
+      apps.${system} = {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/nvim";
+        };
+      };
+
+      # packages.${system}.default = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+      #   configure = {
+      #     customRC = ''
+      #       set number relativenumber
+      #     '';
+      #   };
+      # };
+
+    };
 }
