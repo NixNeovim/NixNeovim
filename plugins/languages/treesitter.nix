4ea80c32509f98df22572e366b14ec59e47e6e9c
Joscha Loos <j.loos@posteo.net>
2022-12-03 19:21:16 +0100

    refactor!: treesitter module

--- plugins/languages/treesitter.nix
+++ plugins/languages/treesitter.nix
@@ -14,35 +14,25 @@ with helpers;
     programs.nixvim.plugins.treesitter = {
       enable = mkEnableOption "Enable tree-sitter syntax highlighting";
 
-      nixGrammars = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Install grammars with Nix (beta)";
-      };
+      installAllGrammars = boolOption true "Install all grammars using nix (recommended)";
 
-      ensureInstalled = mkOption {
-        type = with types; either str (listOf str);
-        default = "all";
-        description = "Either \"all\" or a list of languages";
-      };
+      # parserInstallDir = mkOption {
+      #   type = types.nullOr types.str;
+      #   default = null;
+      #   description = "Location of the parsers to be installed by the plugin (only needed when nixGrammars is disabled)";
+      # };
 
-      parserInstallDir = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = "Location of the parsers to be installed by the plugin (only needed when nixGrammars is disabled)";
-      };
+      # ignoreInstall = mkOption {
+      #   type = types.listOf types.str;
+      #   default = [ ];
+      #   description = "List of parsers to ignore installing (for \"all\")";
+      # };
 
-      ignoreInstall = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = "List of parsers to ignore installing (for \"all\")";
-      };
-
-      disabledLanguages = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = "A list of languages to disable";
-      };
+      # disabledLanguages = mkOption {
+      #   type = types.listOf types.str;
+      #   default = [ ];
+      #   description = "A list of languages to disable";
+      # };
 
       customCaptures = mkOption {
         type = types.attrsOf types.str;
@@ -69,8 +59,8 @@ with helpers;
 
       indent = boolOption false "Enable tree-sitter based indentation (This is the equivalent to indent { enable = true } in the original lua config)";
       folding = boolOption false "Enable tree-sitter based folding";
-      syncInstall = boolOption false "Do not install languages asyncroniously";
-      autoInstall = boolOption true "Install languages automatically";
+      # syncInstall = boolOption false "Do not install languages asyncroniously";
+      # autoInstall = boolOption true "Install languages automatically";
     };
   };
 
@@ -79,7 +69,7 @@ with helpers;
       pluginOptions = {
         highlight = {
           enable = cfg.enable;
-          disable = if (cfg.disabledLanguages != [ ]) then cfg.disabledLanguages else null;
+          # disable = if (cfg.disabledLanguages != [ ]) then cfg.disabledLanguages else null;
 
           custom_captures = if (cfg.customCaptures != { }) then cfg.customCaptures else null;
         };
@@ -100,9 +90,9 @@ with helpers;
             enable = true;
           } else null;
 
-        ensure_installed = if cfg.nixGrammars then [ ] else cfg.ensureInstalled;
-        ignore_install = cfg.ignoreInstall;
-        parser_install_dir = cfg.parserInstallDir;
+        # ensure_installed = if cfg.nixGrammars then [ ] else cfg.ensureInstalled;
+        # ignore_install = cfg.ignoreInstall;
+        # parser_install_dir = cfg.parserInstallDir;
       };
 
       incremental_selection =
@@ -123,27 +113,22 @@ with helpers;
           { enable = true; }
         else null;
 
-      ensure_installed = cfg.ensureInstalled;
-      sync_install = cfg.syncInstall;
-      auto_install = cfg.autoInstall;
+      # ensure_installed = cfg.ensureInstalled;
+      # sync_install = cfg.syncInstall;
+      # auto_install = cfg.autoInstall;
 
     in
     mkIf cfg.enable {
       programs.nixvim = {
-        extraConfigLua =
-          let
-            runtimePath = optionalString (cfg.parserInstallDir != null) "vim.opt.runtimepath:append(\"${cfg.parserInstallDir}\")";
-          in
-          ''
+        extraConfigLua = ''
             require('nvim-treesitter.configs').setup(${helpers.toLuaObject pluginOptions})
-            ${runtimePath}
           '';
 
         extraPlugins = with pkgs;
-          if cfg.nixGrammars then
-            [ (vimExtraPlugins.nvim-treesitter.withPlugins (_: tree-sitter.allGrammars)) ]
+          if cfg.installAllGrammars then
+            [ unstable.vimPlugins.nvim-treesitter.withAllGrammars ]
           else
-            [ vimPlugins.nvim-treesitter ];
+            [ unstable.vimPlugins.nvim-treesitter ];
 
         extraPackages = [ pkgs.tree-sitter pkgs.nodejs ];
 
