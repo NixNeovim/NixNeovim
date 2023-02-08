096ab0b3f423b69b1d94c0b49784224fb6f6d3b4
Joscha Loos <j.loos@posteo.net>
2023-02-08 20:35:51 +0100

    feat: init git-messenger

---
+++ plugins/git/git-messenger.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+let
+
+  name = "git-messenger";
+  pluginUrl = "https://github.com/rhysd/git-messenger.vim";
+
+  helpers = import ../helpers.nix { inherit lib config; };
+
+  # TODO: add options for plugin config (needs changes to mkLuaPlugin)
+
+in
+with helpers;
+mkLuaPlugin {
+  inherit name pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    git-messenger-vim
+  ];
+  defaultRequire = false;
+}
