1c85f987a051be98ee8cc26bba1a1045d5ad5d97
Joscha Loos <j.loos@posteo.net>
2023-05-02 19:45:42 +0200

    test: add plantuml-syntax test

---
+++ tests/plugins/plantuml-syntax.nix
@@ -0,0 +1,41 @@
+{ testHelper, ... }:
+
+{
+  plantuml-syntax-test = { config, lib, pkgs, ... }:
+    {
+      config = {
+
+        programs.nixneovim.plugins = {
+          plantuml-syntax = {
+            enable = true;
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$config" ${
+            pkgs.writeText "init.lua-expected" ''
+              ${testHelper.config.start}
+              -- config for plugin: plantuml-syntax
+              do
+                function setup()
+                  -- test lua pre comment
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+        '';
+      };
+    };
+}
