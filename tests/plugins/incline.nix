4cd70cd1d1e466a77f59039ed56f7c1ebb873727
Joscha Loos <j.loos@posteo.net>
2023-05-11 13:48:04 +0200

    test(incline): add missing lua code to test

--- tests/plugins/incline.nix
+++ tests/plugins/incline.nix
@@ -32,6 +32,7 @@ in {
                 end
                 success, output = pcall(setup) -- execute 'setup()' and catch any errors
                 if not success then
+                  print("Error on setup for plugin: incline")
                   print(output)
                 end
               end
