6bb9f153b91d46135cb79f5d9f58766aefac2fcc
Joscha Loos <j.loos@posteo.net>
2023-05-17 15:39:52 +0200

    feat: newplugin script

--- flake.nix
+++ flake.nix
@@ -62,6 +62,48 @@
             python ./bin/configparser/main.py
           '';
         };
+        newplugin = pkgs.writeShellApplication {
+          name = "newplugin";
+          runtimeInputs = with pkgs; [ ed ];
+          text = ''
+          name="$1"
+          url=$(echo "$2" | sed 's/[\&/]/\\&/g')
+          plugin="$3"
+
+          [ -z "$name" ] && exit 1
+          [ -z "$url" ] && exit 1
+          [ -z "$plugin" ] && exit 1
+
+          plugin_path="plugins/utils/$name.nix"
+          plugin_test_path="tests/plugins/$name.nix"
+
+          echo Copy template
+          cp ./plugin_template_minimal.nix "$plugin_path"
+
+          echo Replace names
+          sed -i "s/PLUGIN_NAME/$name/" "$plugin_path"
+          sed -i "s/PLUGIN_URL/$url/" "$plugin_path"
+
+          echo Insert plugin
+          ed "$plugin_path" <<EOF
+          g/add neovim plugin here/p
+          a
+              $plugin
+          .
+          w
+          q
+          EOF
+
+          echo Copy test template
+          cp ./test_template.nix "$plugin_test_path"
+
+          echo Replace names
+          sed -i "s/NAME/$name/" "$plugin_test_path"
+
+          echo Adding new files to git
+          git add "$plugin_path" "$plugin_test_path"
+          '';
+        };
       };
 
       nixosModules = {
