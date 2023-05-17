1dd2c314e7dc665672c2cdad464d55862b9a3deb
Joscha Loos <j.loos@posteo.net>
2023-05-17 11:11:31 +0200

    feat: add plugins easier with a new script

---
+++ add_plugin.sh
@@ -0,0 +1,34 @@
+set -o errexit
+set -o nounset
+set -o pipefail
+
+name="$1"
+url=$(echo "$2" | sed 's/[\&/]/\\&/g')
+plugin="$3"
+
+
+[ -z "$name" ] && exit 1
+[ -z "$url" ] && exit 1
+[ -z "$plugin" ] && exit 1
+
+plugin_path="plugins/utils/$name.nix"
+
+echo Copy template
+cp ./plugin_template_minimal.nix "$plugin_path"
+
+echo Replace names
+sed -i "s/PLUGIN_NAME/$name/" "$plugin_path"
+sed -i "s/PLUGIN_URL/$url/" "$plugin_path"
+
+echo Insert plugin
+ed "$plugin_path" <<EOF
+g/add neovim plugin here/p
+a
+    $plugin
+.
+w
+q
+EOF
+
+
+
