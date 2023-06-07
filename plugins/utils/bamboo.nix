486585c27842f48ef7198d23137c441eddbd88d4
Joscha Loos <j.loos@posteo.net>
2023-06-07 15:54:46 +0200

    feat(colorschemes): add bamboo

---
+++ plugins/utils/bamboo.nix
@@ -0,0 +1,69 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+let
+
+  name = "bamboo";
+  pluginUrl = "https://github.com/ribru17/bamboo.nvim";
+
+  helpers = import ../../helper { inherit pkgs lib config; };
+
+  inherit (helpers.customOptions)
+    strOption
+    listOption
+    enumOption
+    boolOption;
+
+  moduleOptions = {
+    style = enumOption [ "vulgaris" "multiplex" ] "vulgaris" "";
+    # Choose between 'vulgaris' (regular) and 'multiplex' (greener)
+    # Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
+    toggleStyleList = listOption [
+      "vulgaris"
+      "multiplex"
+    ] "";
+    # List of styles to toggle between (this option is essentially pointless now but will become useful if more style variations are added)
+    transparent = boolOption false "List of styles to toggle between (this option is essentially pointless now but will become useful if more style variations are added)";
+    # Show/hide background
+    termColors = boolOption true "Show/hide background";
+    # Change terminal color as per the selected theme style
+    endingTildes = boolOption false "Change terminal color as per the selected theme style";
+    # Show the end-of-buffer tildes. By default they are hidden
+    cmpItemkindReverse = boolOption false "Show the end-of-buffer tildes. By default they are hidden";
+    # reverse item kind highlights in cmp menu
+    # Change code style
+    # Options are italic, bold, underline, none
+    # You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    codeStyle = {
+      comments = strOption "italic" "You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'";
+      keywords = strOption "none" "You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'";
+      functions = strOption "none" "You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'";
+      strings = strOption "none" "You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'";
+      variables = strOption "none" "You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'";
+    };
+    # Lualine options
+    lualine = {
+      transparent = boolOption false "";
+    };
+    # Plugins Config --
+    diagnostics = {
+      darker = boolOption false "";
+      # darker colors for diagnostic
+      undercurl = boolOption true "darker colors for diagnostic";
+      # use undercurl instead of underline for diagnostics
+      background = boolOption true "use undercurl instead of underline for diagnostics";
+      # use background color for virtual text
+    };
+  };
+
+in
+with helpers;
+mkLuaPlugin {
+  inherit name moduleOptions pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    bamboo-nvim
+  ];
+  isColorscheme = true;
+}
---
+++ tests/plugins/bamboo.nix
@@ -0,0 +1,71 @@
+{ testHelper, ... }:
+
+let
+  name = "bamboo";
+  nvimTestCommand = "colorscheme bamboo"; # Test command to check if plugin is loaded
+in {
+  "${name}-test" = { config, lib, pkgs, ... }:
+    {
+      config = {
+
+        programs.nixneovim.colorschemes = {
+          "${name}" = {
+            enable = true;
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$normalizedConfig" ${
+            pkgs.writeText "init.lua-expected" ''
+              ${testHelper.config.start}
+              -- config for plugin: ${name}
+              do
+                function setup()
+                  -- test lua pre comment
+                  require('bamboo').setup {
+                    ["cmp_itemkind_reverse"] = false,
+                    ["code_style"] = {
+                      ["comments"] = "italic",
+                      ["functions"] = "none",
+                      ["keywords"] = "none",
+                      ["strings"] = "none",
+                      ["variables"] = "none"
+                    },
+                    ["diagnostics"] = {
+                      ["background"] = true,
+                      ["darker"] = false,
+                      ["undercurl"] = true
+                    },
+                    ["ending_tildes"] = false,
+                    ["lualine"] = { ["transparent"] = false },
+                    ["style"] = "vulgaris",
+                    ["term_colors"] = true,
+                    ["toggle_style_list"] = {
+                      "vulgaris",
+                      "multiplex"
+                    },
+                    ["transparent"] = false
+                  }
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print("Error on setup for plugin: ${name}")
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+
+          start_vim -c "${nvimTestCommand}"
+        '';
+      };
+    };
+}
