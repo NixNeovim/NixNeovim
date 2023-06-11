ba052377f7fe517210c460623c7a9c3a518f9dd5
Jalil David Salamé Messina <jalil.salame@gmail.com>
2023-06-11 17:18:35 +0200

    fix(flake.nix): Switch nmd repo to SourceHut
    The original GitLab repo has been archived, and development is being
    done in SourceHut

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nmd = {
-      url = "gitlab:rycee/nmd";
+      url = "sourcehut:~rycee/nmd";
       flake = false;
     };
     nmt = {
