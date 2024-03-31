9df52e45ecf6ee28ec7095200e9a421785f289e8
Joscha Loos <j.loos@posteo.net>
2024-03-31 18:58:25 +0200

    feat: port ledger plugin to mkLuaPlugin and add test

---
+++ tests/integration/environments/ledger.nix
@@ -0,0 +1,52 @@
+{ testHelper, lib }:
+
+let
+  name = "ledger";
+  nvimTestCommand = ""; # Test command to check if plugin is loaded
+in {
+  "${name}-test" = { config, lib, pkgs, ... }:
+
+    {
+      config = {
+
+        programs.nixneovim.plugins = {
+          "${name}" = {
+            enable = true;
+            maxWidth = 80;
+            fillString = "   -";
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$normalizedConfig" ${
+            pkgs.writeText "init.lua-expected" ''
+              ${testHelper.config.start}
+              -- config for plugin: ${name}
+              do
+                function setup()
+                  -- test lua pre comment
+                  vim.g.ledger_fillstring = "    -"
+                  vim.g.ledger_maxwidth = 80
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print("Error on setup for plugin: ${name}")
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+
+          start_vim -c "${nvimTestCommand}"
+        '';
+      };
+    };
+}
