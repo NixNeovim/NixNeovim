6c4ece53d7819cbeec839ac5f9b223a0f258f667
Joscha Loos <j.loos@posteo.net>
2024-05-15 13:34:12 +0200

    feat: move surround to mkLuaPlugin

---
+++ tests/integration/plugins/surround.nix
@@ -0,0 +1,49 @@
+{ testHelper, lib }:
+
+let
+  name = "surround";
+  nvimTestCommand = ""; # Test command to check if plugin is loaded (optional)
+in {
+  "${name}-test" = { config, lib, pkgs, ... }:
+
+    {
+      config = {
+
+        programs.nixneovim.plugins = {
+          "${name}" = {
+            enable = true;
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$normalizedConfig" ${
+            pkgs.writeText "init.lua-expected" /* lua */ ''
+              ${testHelper.config.start}
+              -- config for plugin: ${name}
+              do
+                function setup()
+                  -- test lua pre comment
+                  require('nvim-surround').setup {}
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print("Error on setup for plugin: ${name}")
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+
+          start_vim -c "${nvimTestCommand}"
+        '';
+      };
+    };
+}
