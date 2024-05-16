a0dcd279644c07e38160acb4ab9a219daec822d1
Joscha Loos <j.loos@posteo.net>
2024-05-16 23:53:41 +0200

    feat: move emmet to mkLuaPlugin

--- src/plugins/emmet.nix
+++ src/plugins/emmet.nix
@@ -1,38 +1,48 @@
 { lib, pkgs, helpers, config }:
-with lib;
 let
 
-  inherit (helpers.deprecated)
-    mkDefaultOpt
-    mkPlugin;
 
-  eitherAttrsStrInt = with types;
+  inherit (helpers.generator)
+     mkLuaPlugin;
+
+  inherit (lib.types)
+    either
+    int
+    str
+    attrsOf;
+
+  name = "emmet";
+  pluginUrl = "https://github.com/emmetio/emmet";
+
+  # only needed when the name of the plugin does not match the
+  # name in the 'require("<...>")' call. For example, the plugin 'comment-frame'
+  # has to be called with 'require("nvim-comment-frame")'
+  # in such a case add 'pluginName = "nvim-comment-frame"'
+  # pluginName = ""
+
+  inherit (helpers.custom_options)
+    strOption
+    attrsOfOption
+    enumOption;
+
+  eitherAttrsStrInt =
     let
       strInt = either str int;
     in either strInt (attrsOf (either strInt (attrsOf strInt)));
 
-in mkPlugin { inherit config lib; } {
-  name = "emmet";
-  description = "Enable emmet";
-  extraPlugins = [ pkgs.vimPlugins.emmet-vim ];
-
-  options = {
-    mode = mkDefaultOpt {
-      type = types.enum [ "i" "n" "v" "a" ];
-      global = "user_emmet_mode";
-      description = "Mode where emmet will enable";
-    };
-
-    leader = mkDefaultOpt {
-      type = types.str;
-      global = "user_emmet_leader_key";
-      description = "Set leader key";
-    };
-
-    settings = mkDefaultOpt {
-      type = types.attrsOf (types.attrsOf eitherAttrsStrInt);
-      global = "user_emmet_settings";
-      description = "Emmet settings";
-    };
+  moduleOptionsVim = {
+    # add module options here
+    mode = enumOption [ "i" "n" "v" "a" ] "n" "Mode where emmet will enable";
+    leaderKey = strOption "<C-Y>" "Set leader key";
+    settings = attrsOfOption (attrsOf eitherAttrsStrInt) {} "Emmet settings";
   };
+
+in mkLuaPlugin {
+  inherit name moduleOptionsVim pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    emmet-vim
+  ];
+  moduleOptionsVimPrefix = "user_emmet_";
+  defaultRequire = false;
 }
