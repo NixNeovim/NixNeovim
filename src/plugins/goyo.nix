20d666233a978759a467839f560037286cc12bc1
Joscha Loos <j.loos@posteo.net>
2024-05-16 18:44:14 +0200

    feat: move goyo to mkLuaPlugin

--- src/plugins/goyo.nix
+++ src/plugins/goyo.nix
@@ -1,36 +1,53 @@
-{ lib, pkgs, helpers, config }:
+{ pkgs, lib, helpers, ... }:
 
 let
 
-  inherit (helpers.deprecated)
-    mkPlugin
-    mkDefaultOpt;
+  inherit (helpers.generator)
+     mkLuaPlugin;
 
-  inherit (lib)
-    types;
-
-in mkPlugin { inherit config lib; } {
   name = "goyo";
-  description = "Enable goyo.vim";
-  extraPlugins = [ pkgs.vimPlugins.goyo-vim ];
+  pluginUrl = "https://github.com/junegunn/goyo.vim";
 
-  options = {
-    width = mkDefaultOpt {
-      description = "Width";
-      global = "goyo_width";
-      type = types.int;
-    };
+  # only needed when the name of the plugin does not match the
+  # name in the 'require("<...>")' call. For example, the plugin 'comment-frame'
+  # has to be called with 'require("nvim-comment-frame")'
+  # in such a case add 'pluginName = "nvim-comment-frame"'
+  # pluginName = ""
 
-    height = mkDefaultOpt {
-      description = "Height";
-      global = "goyo_height";
-      type = types.int;
-    };
+  inherit (helpers.custom_options)
+    strOption
+    listOption
+    enumOption
+    intOption
+    boolOption;
 
-    showLineNumbers = mkDefaultOpt {
-      description = "Show line numbers when in Goyo mode";
-      global = "goyo_linenr";
-      type = types.bool;
-    };
+  moduleOptionsVim = {
+    # add module options here
+    width = intOption 80 "";
+    height = intOption 85 "";
+    linenr = intOption 0 "Show line numbers when in Goyo mode";
   };
+
+
+in mkLuaPlugin {
+
+# Consider the following additional options:
+#
+# extraDescription ? ""           # description added to the enable function
+# extraPackages ? [ ]             # non-plugin packages
+# extraConfigLua ? ""             # lua config added to the init.vim
+# extraConfigVim ? ""             # vim config added to the init.vim
+# defaultRequire ? true           # add default requrie string?
+# extraOptions ? {}               # extra vim options like line numbers, etc
+# extraNixNeovimConfig ? {}       # extra config applied to 'programs.nixneovim'
+# isColorscheme ? false           # If enabled, plugin will be added to 'nixneovim.colorschemes' instead of 'nixneovim.plugins'
+# configConverter ? camelToSnake  # Specify the config name converter, default expects camelCase and converts that to snake_case
+
+  inherit name moduleOptionsVim pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    goyo-vim
+  ];
+  moduleOptionsVimPrefix = "goyo_";
+  defaultRequire = false;
 }
---
+++ tests/integration/plugins/goyo.nix
@@ -0,0 +1,52 @@
+{ testHelper, lib }:
+
+let
+  name = "goyo";
+  nvimTestCommand = ""; # Test command to check if plugin is loaded (optional)
+in {
+  "${name}-test" = { config, lib, pkgs, ... }:
+
+    {
+      config = {
+
+        programs.nixneovim.plugins = {
+          "${name}" = {
+            enable = true;
+            height = 50;
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$normalizedConfig" ${
+            pkgs.writeText "init.lua-expected" /* lua */ ''
+              ${testHelper.config.start}
+              -- config for plugin: ${name}
+              do
+                function setup()
+                  -- test lua pre comment
+                  vim.g.goyo_height = 50
+                  vim.g.goyo_linenr = 0
+                  vim.g.goyo_width = 80
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print("Error on setup for plugin: ${name}")
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+
+          start_vim -c "${nvimTestCommand}"
+        '';
+      };
+    };
+}
