5a1f04bb8c786a349d499893470e708fa0d19774
Joscha Loos <j.loos@posteo.net>
2024-06-05 22:55:26 +0200

    chore: add more explaination to plugin template

--- plugin_template_minimal.nix
+++ plugin_template_minimal.nix
@@ -40,6 +40,9 @@ in mkLuaPlugin {
 # extraNixNeovimConfig ? {}       # extra config applied to 'programs.nixneovim'
 # isColorscheme ? false           # If enabled, plugin will be added to 'nixneovim.colorschemes' instead of 'nixneovim.plugins'
 # configConverter ? camelToSnake  # Specify the config name converter, default expects camelCase and converts that to snake_case
+# moduleOptions                   # define (lua) configuration options for the plugin here
+# moduleOptionsVim                # define (vim) configuration options for the plugin here
+# moduleOptionsVimPrefix          # when using 'moduleOptionsVim' you can use this to define the options prefix. For example, "NERD" (for NerdCommenter), or "ledger_" (for ledger)
 
   inherit name moduleOptions pluginUrl;
   extraPlugins = with pkgs.vimExtraPlugins; [
