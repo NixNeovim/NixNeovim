001c53956ee8fc0941a0e6f99c874151cdcdb49d
Joscha Loos <j.loos@posteo.net>
2024-06-05 22:55:39 +0200

    feat: move tagbar from mkplugin to mkluaplugin

--- src/plugins/tagbar.nix
+++ src/plugins/tagbar.nix
@@ -1,12 +1,49 @@
-{ pkgs, helpers, config, lib }:
+{ pkgs, lib, helpers, ... }:
 
 let
-  inherit (helpers.deprecated)
-      mkPlugin;
-in mkPlugin { inherit lib config; } {
+
+  inherit (helpers.generator)
+     mkLuaPlugin;
+
   name = "tagbar";
-  description = "Enable tagbar";
+  pluginUrl = "https://github.com/preservim/tagbar";
+
+  # only needed when the name of the plugin does not match the
+  # name in the 'require("<...>")' call. For example, the plugin 'comment-frame'
+  # has to be called with 'require("nvim-comment-frame")'
+  # in such a case add 'pluginName = "nvim-comment-frame"'
+  # pluginName = ""
+
+  inherit (helpers.custom_options)
+    strOption
+    listOption
+    enumOption
+    intOption
+    boolOption;
+
+  moduleOptionsVim = {
+    # add module options here
+  };
+
+in mkLuaPlugin {
+
+# Consider the following additional options:
+#
+# extraDescription ? ""           # description added to the enable function
+# extraPackages ? [ ]             # non-plugin packages
+# extraConfigLua ? ""             # lua config added to the init.vim
+# extraConfigVim ? ""             # vim config added to the init.vim
+# defaultRequire ? true           # add default requrie string?
+# extraOptions ? {}               # extra vim options like line numbers, etc
+# extraNixNeovimConfig ? {}       # extra config applied to 'programs.nixneovim'
+# isColorscheme ? false           # If enabled, plugin will be added to 'nixneovim.colorschemes' instead of 'nixneovim.plugins'
+# configConverter ? camelToSnake  # Specify the config name converter, default expects camelCase and converts that to snake_case
+
+  inherit name moduleOptionsVim pluginUrl;
   extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
     tagbar
   ];
+  defaultRequire = false;
+  moduleOptionsVimPrefix = "tagbar_";
 }
