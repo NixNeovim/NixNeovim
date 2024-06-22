7143f4e291c55a7a8451052f19bd1e1e2bee4b0a
Joscha Loos <j.loos@posteo.net>
2024-06-22 21:33:37 +0200

    feat: move airline to mkLuaPlugin

--- src/plugins/airline.nix
+++ src/plugins/airline.nix
@@ -1,78 +1,89 @@
 { pkgs, lib, helpers, config }:
-with lib;
-let
-  cfg = config.programs.nixneovim.plugins.airline;
 
-  sectionType = with types; nullOr (oneOf [ str (listOf str) ]);
-  sectionOption = mkOption {
+let
+
+  inherit (helpers.generator)
+     mkLuaPlugin;
+
+  name = "airline";
+  pluginUrl = "https://github.com/vim-airline/vim-airline";
+
+  cfg = config.programs.nixneovim.plugins.${name};
+
+  sectionType = with lib.types; nullOr (oneOf [ str (listOf str) ]);
+
+  sectionOption = lib.mkOption {
     default = null;
     type = sectionType;
     description = "Configuration for this section. Can be either a statusline-format string or a list of modules to be passed to airline#section#create_*.";
   };
-in
-{
-  options = {
-    programs.nixneovim.plugins.airline = {
-      enable = mkEnableOption "airline";
 
-      extensions = mkOption {
-        default = null;
-        type = with types; nullOr attrs;
-        description = "A list of extensions and their configuration";
-      };
+  inherit (helpers.custom_options)
+    strOption
+    attrsOption
+    listOption
+    enumOption
+    intOption
+    boolOption;
 
-      onTop = mkOption {
-        default = false;
-        type = types.bool;
-        description = "Whether to show the statusline on the top instead of the bottom";
-      };
+  moduleOptionsVim = {
+    # add module options here
+    extensions = attrsOption {} "A list of extensions and their configuration";
+    onTop = boolOption false "Whether to show the statusline on the top instead of the bottom";
 
-      sections = mkOption {
-        description = "Statusbar sections";
-        default = null;
-        type = with types; nullOr (submodule {
-          options = {
-            a = sectionOption;
-            b = sectionOption;
-            c = sectionOption;
-            x = sectionOption;
-            y = sectionOption;
-            z = sectionOption;
-          };
-        });
-      };
-
-      powerline = mkOption {
-        default = false;
-        type = types.bool;
-        description = "Whether to use powerline symbols";
-      };
-
-      theme = mkOption {
-        default = config.programs.nixneovim.colorscheme;
-        type = with types; nullOr str;
-        description = "The theme to use for vim-airline. If set, vim-airline-themes will be installed.";
-      };
+    sections = lib.mkOption {
+      description = "Statusbar sections";
+      default = null;
+      type = with lib.types; nullOr (submodule {
+        options = {
+          a = sectionOption;
+          b = sectionOption;
+          c = sectionOption;
+          x = sectionOption;
+          y = sectionOption;
+          z = sectionOption;
+        };
+      });
     };
+
+    powerline = boolOption false "Whether to use powerline symbols";
+
+    theme = strOption "" "The theme to use for vim-airline. If set, vim-airline-themes will be installed.";
   };
+in mkLuaPlugin {
 
-  config =
-    let
-      sections = { };
-    in
-    mkIf cfg.enable {
-      programs.nixneovim = {
-        extraPlugins = with pkgs.vimPlugins; [
-          vim-airline
-        ] ++ optional (!isNull cfg.theme) vim-airline-themes;
-        globals = {
-          airline.extensions = cfg.extensions;
+# Consider the following additional options:
+#
+# extraDescription ? ""           # description added to the enable function
+# extraPackages ? [ ]             # non-plugin packages
+# extraConfigLua ? ""             # lua config added to the init.vim
+# extraConfigVim ? ""             # vim config added to the init.vim
+# defaultRequire ? true           # add default requrie string?
+# extraOptions ? {}               # extra vim options like line numbers, etc
+# extraNixNeovimConfig ? {}       # extra config applied to 'programs.nixneovim'
+# isColorscheme ? false           # If enabled, plugin will be added to 'nixneovim.colorschemes' instead of 'nixneovim.plugins'
+# configConverter ? camelToSnake  # Specify the config name converter, default expects camelCase and converts that to snake_case
+# moduleOptions                   # define (lua) configuration options for the plugin here
+# moduleOptionsVim                # define (vim) configuration options for the plugin here
+# moduleOptionsVimPrefix          # when using 'moduleOptionsVim' you can use this to define the options prefix. For example, "NERD" (for NerdCommenter), or "ledger_" (for ledger)
 
-          airline_statusline_ontop = mkIf cfg.onTop 1;
-          airline_powerline_fonts = mkIf (cfg.powerline) 1;
-
-          airline_theme = mkIf (!isNull cfg.theme) cfg.theme;
-        } // sections;
-      };
-    };
+  inherit name moduleOptionsVim pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    vim-airline
+  ] ++ optional (!isNull cfg.theme) vim-airline-themes;
 }
+  # config =
+  #     programs.nixneovim = {
+  #       extraPlugins = with pkgs.vimPlugins; [
+  #       ] ++ optional (!isNull cfg.theme) vim-airline-themes;
+  #       globals = {
+  #         airline.extensions = cfg.extensions;
+
+  #         airline_statusline_ontop = mkIf cfg.onTop 1;
+  #         airline_powerline_fonts = mkIf (cfg.powerline) 1;
+
+  #         airline_theme = mkIf (!isNull cfg.theme) cfg.theme;
+  #       };
+  #     };
+  #   };
---
+++ tests/integration/plugins/airline.nix
@@ -0,0 +1,48 @@
+{ testHelper, lib }:
+
+let
+  name = "airline";
+  nvimTestCommand = ""; # Test command to check if plugin is loaded (optional)
+in {
+  "${name}-test" = { config, lib, pkgs, ... }:
+
+    {
+      config = {
+
+        programs.nixneovim.plugins = {
+          "${name}" = {
+            enable = true;
+            extraLua.pre = ''
+              -- test lua pre comment
+            '';
+            extraLua.post = ''
+              -- test lua post comment
+            '';
+          };
+        };
+
+        nmt.script = testHelper.moduleTest ''
+          assertDiff "$normalizedConfig" ${
+            pkgs.writeText "init.lua-expected" /* lua */ ''
+              ${testHelper.config.start}
+              -- config for plugin: ${name}
+              do
+                function setup()
+                  -- test lua pre comment
+                  -- test lua post comment
+                end
+                success, output = pcall(setup) -- execute 'setup()' and catch any errors
+                if not success then
+                  print("Error on setup for plugin: ${name}")
+                  print(output)
+                end
+              end
+              ${testHelper.config.end}
+            ''
+          }
+
+          start_vim -c "${nvimTestCommand}"
+        '';
+      };
+    };
+}
