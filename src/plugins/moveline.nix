933cdcb5991d3a48578d7e4df96ff7ef4f7bd529
Joscha Loos <j.loos@posteo.net>
2024-08-15 12:55:13 +0200

    feat: add moveline

---
+++ src/plugins/moveline.nix
@@ -0,0 +1,52 @@
+{ pkgs, lib, helpers, ... }:
+
+let
+
+  inherit (helpers.generator)
+     mkLuaPlugin;
+
+  name = "moveline";
+  pluginUrl = "https://github.com/willothy/moveline.nvim";
+
+  # only needed when the name of the plugin does not match the
+  # name in the 'require("<...>")' call. For example, the plugin 'comment-frame'
+  # has to be called with 'require("nvim-comment-frame")'
+  # in such a case add 'pluginName = "nvim-comment-frame"'
+  # pluginName = ""
+
+  inherit (helpers.custom_options)
+    strOption
+    listOption
+    enumOption
+    intOption
+    boolOption;
+
+  moduleOptions = {
+    # add module options here
+  };
+
+
+
+in mkLuaPlugin {
+
+# Consider the following additional options:
+#
+# extraDescription ? ""           # description added to the enable function
+# extraPackages ? [ ]             # non-plugin packages
+# extraConfigLua ? ""             # lua config added to the init.vim
+# extraConfigVim ? ""             # vim config added to the init.vim
+# defaultRequire ? true           # add default requrie string?
+# extraOptions ? {}               # extra vim options like line numbers, etc
+# extraNixNeovimConfig ? {}       # extra config applied to 'programs.nixneovim'
+# isColorscheme ? false           # If enabled, plugin will be added to 'nixneovim.colorschemes' instead of 'nixneovim.plugins'
+# configConverter ? camelToSnake  # Specify the config name converter, default expects camelCase and converts that to snake_case
+# moduleOptions                   # define (lua) configuration options for the plugin here
+# moduleOptionsVim                # define (vim) configuration options for the plugin here
+# moduleOptionsVimPrefix          # when using 'moduleOptionsVim' you can use this to define the options prefix. For example, "NERD" (for NerdCommenter), or "ledger_" (for ledger)
+
+  inherit name moduleOptions pluginUrl;
+  extraPlugins = with pkgs.vimExtraPlugins; [
+    # add neovim plugin here
+    moveline-nvim
+  ];
+}
